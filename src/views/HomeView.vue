dc83934ba45e08c567cd740b8077422773dcd79b
Ivan Adamovic <ivan.adamovic.21@singimail.rs>
2025-05-05 15:46:48 +0200

    Editing/Deleting files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>Home Page | Praktikum Sistemi E-Poslovanja</h1>
 </template>
